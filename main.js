e6e7ae5b1c64af3434726fd5f5ebc8f439d1db30
Lucas Miguel <106555557+LucasMiguel0@users.noreply.github.com>
2024-04-03 10:43:17 -0300

    3/4

--- main.js
+++ main.js
@@ -1,10 +1,10 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for(let i=0;1 <botoes.length;i++){
+for(let i=0;1 < botoes.length;i++){
     botoes[i].onclick = function(){
 
-        for(let j=0;j<botoes.length;j++){
+        for(let j=0;j < botoes.length;j++){
             botoes[j].classList.remove("ativo");
             textos[j].classList.remove("ativo");
         }
